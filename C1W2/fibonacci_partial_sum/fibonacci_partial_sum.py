bd353b11974378dd6a22e949f50b954c3cb7efc4
Bai Xue <xuexx168@umn.edu>
2017-05-09 17:31:49 -0500

    Complete week2 assignment

--- C1W2/fibonacci_partial_sum/fibonacci_partial_sum.py
+++ C1W2/fibonacci_partial_sum/fibonacci_partial_sum.py
@@ -1,22 +1,39 @@
 # Uses python3
 import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    sum = 0
+def fibonacci_sum(n):
+    if n <= 1:
+        return n
 
-    current = 0
-    next  = 1
+    x=n%60
+    y=n//60
+    f0=0
+    f1=1
+    sum=1
+    sum1=1
+    for i in range(2,60):
+        f2=(f0+f1)%10
+        f0=f1
+        f1=f2
+        sum=(sum+f2)%10
+        if i<=x:
+            sum1=(sum1+f2)%10
 
-    for i in range(to + 1):
-        if i >= from_:
-            sum += current
+    sum2=(y*sum)%10
+    if x==0:
+        return sum2
+    else:
+        return (sum2+sum1)%10
 
-        current, next = next, current + next
-
-    return sum % 10
-
+def fibonacci_partial_sum(from_, to):
+    a=fibonacci_sum(from_-1)
+    b=fibonacci_sum(to)
+    if a>b:
+        return 10+b-a
+    else:
+        return b-a
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.readline();
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    print(fibonacci_partial_sum(from_, to))
