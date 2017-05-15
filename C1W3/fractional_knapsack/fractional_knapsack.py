e690fb2b4d58c57134992746510ee27f4af540dd
Bai Xue <xuexx168@umn.edu>
2017-05-15 14:25:26 -0500

    Problem 1 done

--- C1W3/fractional_knapsack/fractional_knapsack.py
+++ C1W3/fractional_knapsack/fractional_knapsack.py
@@ -1,15 +1,49 @@
 # Uses python3
 import sys
 
+def cparray(b,d,f,iBegin,iEnd,a,c,e):
+    for k in range(iBegin,iEnd):
+        a[k]=b[k]
+        c[k]=d[k]
+        e[k]=f[k]   
+
+def merge(a,c,e,iBegin,iMiddle,iEnd,b,d,f):
+    i0=iBegin
+    i1=iMiddle
+    for j in range(iBegin,iEnd):
+        print i0
+        if i0<iMiddle and (i1>=iEnd or a[i0]>=a[i1]):
+            b[j]=a[i0]
+            d[j]=c[i0]
+            f[j]=e[i0]
+            i0=i0+1
+        else:
+            b[j]=a[i1]
+            d[j]=c[i1]
+            f[j]=e[i1]
+            i1=i1+1
+#        print b[j]
+
+def splitmerge(a,c,e,iBegin,iEnd,b,d,f):
+    if iEnd-iBegin<2:
+        return
+    iMiddle=(iBegin+iEnd)/2
+    splitmerge(a,c,e,iBegin,iMiddle,b,d,f)
+    splitmerge(a,c,e,iMiddle,iEnd,b,d,f)
+    merge(a,c,e,iBegin,iMiddle,iEnd,b,d,f)
+    cparray(b,d,f,iBegin,iEnd,a,c,e)
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
     vw=[]
-    for i in range(0,n)
-        vw.append(values[i]/weights[i])
-
-
-    # write your code here
-
+    for i in range(0,n):
+        vw.append(float(values[i])/weights[i])
+#    print vw
+    vwS=vw
+    weightsS=weights
+    valuesS=values
+    splitmerge(vw,weights,values,0,n,vwS,weightsS,valuesS)
+#    print vw,weights,values
     return value
 
 
---
+++ C1W3/fractional_knapsack/fractional_knapsack_bubble.py
@@ -0,0 +1,43 @@
+# Uses python3
+import sys
+
+def get_optimal_value(capacity, weights, values):
+    value = 0.
+    vw=[]
+    for i in range(0,n):
+        vw.append(float(values[i])/weights[i])
+
+    for i in range(0,n-2):
+        for j in range(i,n-1):
+            if vw[j]<vw[j+1]:
+                tmp=vw[j]
+                vw[j]=vw[j+1]
+                vw[j+1]=vw[j]
+                tmp=values[j]
+                values[j]=values[j+1]
+                values[j+1]=tmp
+                tmp=weights[j]
+                weights[j]=weights[j+1]
+                weights[j+1]=tmp               
+ 
+    sw=0.0   
+    for i in range(0,n):
+        if sw<capacity:
+            if (capacity-sw)>=weights[i]:
+                sw=sw+weights[i]
+                value=value+values[i]
+            else:
+                frac=(capacity-sw)/weights[i]
+                sw=sw+frac*weights[i]
+                value=value+frac*values[i]
+
+    return value
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
