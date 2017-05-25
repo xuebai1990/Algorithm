72360f31f78a5b6afb09fcd456ad323e478d9369
Bai Xue <xuexx168@umn.edu>
2017-05-25 12:38:35 -0500

    W4 p1

--- C1W4/binary_search/binary_search.py
+++ C1W4/binary_search/binary_search.py
@@ -4,6 +4,15 @@ import sys
 def binary_search(a, x):
     left, right = 0, len(a)
     # write your code here
+    while left<right:
+        mid=(left+right)//2
+        if a[mid]==x:
+            return mid
+        elif x<a[mid]:
+            right=mid
+        else:
+            left=mid+1
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +28,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
