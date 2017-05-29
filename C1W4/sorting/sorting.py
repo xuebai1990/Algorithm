86ebe733c7d429e6c706c2cabc9adc10c2fa3233
Bai Xue <xuexx168@umn.edu>
2017-05-28 19:01:36 -0500

    C1W4P3

--- C1W4/sorting/sorting.py
+++ C1W4/sorting/sorting.py
@@ -4,7 +4,29 @@ import random
 
 def partition3(a, l, r):
     #write your code here
-    pass
+    if r - l <= 1:
+        if a[l] > a[r]:
+            a[l], a[r] = a[r], a[l]
+            return l, r
+
+    x = a[l]
+    j = l
+    k = l
+    for i in range(l + 1, r + 1):
+        if a[i] < x:
+            j += 1
+            k += 1
+            a[i], a[j] = a[j], a[i]
+        elif a[i] == x:
+            k += 1
+            a[i], a[k] = a[k], a[i]
+    a[l], a[j] = a[j], a[l]
+    print(a[j])
+    for i in range(l,r+1):
+        print(a[i],end=' ') 
+        if i == r:
+            print("\n")
+    return j, k
 
 def partition2(a, l, r):
     x = a[l]
@@ -23,14 +45,14 @@ def randomized_quick_sort(a, l, r):
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
     #use partition3
-    m = partition2(a, l, r)
+    m, n = partition3(a, l, r)
     randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    randomized_quick_sort(a, n + 1, r);
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     randomized_quick_sort(a, 0, n - 1)
-    for x in a:
-        print(x, end=' ')
+#    for x in a:
+#        print(x, end=' ')
