020fffffafc69c1a8dcc5cec63b7090341c211cc
Bai Xue <bai@ums-10-155-27-68.wireless.umn.edu>
2017-06-11 20:13:34 -0500

    Solve tree_height

--- C2W1/tree_height/tree-height.py
+++ C2W1/tree_height/tree-height.py
@@ -4,26 +4,38 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
+class Node:
+    def __init__(self):
+        self.child = []
+
 class TreeHeight:
         def read(self):
                 self.n = int(sys.stdin.readline())
                 self.parent = list(map(int, sys.stdin.readline().split()))
+                self.nd = []
+                for i in range(self.n):
+                    x = Node()
+                    self.nd.append(x)
+                for i in range(self.n):
+                    if self.parent[i] == -1:
+                        self.root = i
+                    else:
+                        self.nd[self.parent[i]].child.append(i) 
 
-        def compute_height(self):
+        def compute_height(self, num):
                 # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                if len(self.nd[num].child) == 0:
+                    return 1
+                else:
+                    a = []
+                    for i in range(len(self.nd[num].child)):
+                        a.append(1+self.compute_height(self.nd[num].child[i]))
+                    return max(a)
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
+    tree = TreeHeight()
+    tree.read()
+    print(tree.compute_height(tree.root))
 
+#main()
 threading.Thread(target=main).start()
