020c19cabdc0077472555d2d7ff3b8256239f0fc
Bai Xue <bai@ums-10-155-27-68.wireless.umn.edu>
2017-07-11 15:44:55 -0500

    C2W4 finished

--- C2W4/hash_substring/hash_substring.py
+++ C2W4/hash_substring/hash_substring.py
@@ -1,4 +1,5 @@
 # python3
+import random
 
 def read_input():
     return (input().rstrip(), input().rstrip())
@@ -6,12 +7,36 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
+def hash(s, p, x):
+    ans = 0
+    for c in reversed(s):
+        ans = (ans * x + ord(c)) % p
+    return ans % p
+
+def pre_hash(T, P, prime, x, H):
+    l = len(T) - len(P) + 1
+    s = T[(l - 1):len(T)]
+    H[l - 1] = hash(s, prime, x)
+    y = 1
+    for i in range(len(P)):
+        y = (y * x) % prime
+    for i in range(l - 2, -1, -1):
+        H[i] = (x * H[i + 1] + ord(T[i]) - y * ord(T[i + len(P)])) % prime
+
 def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+    p = 1000000007
+    x = random.randint(1, p - 1)
+    result = []
+    pHash = hash(pattern, p, x)
+    H = [0 for i in range(len(text) - len(pattern) + 1)]
+    pre_hash(text, pattern, p, x, H)
+    for i in range(len(text) - len(pattern) + 1):
+        if pHash != H[i]:
+            continue
+        st = text[i:(i+len(pattern))]
+        if st == pattern:
+            result.append(i)
+    return result
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
